ca1daba343623c165dcabd4aae1aeea87193e095
Lilliam Solis <lilliam.solis.avila@est.una.ac.cr>
2023-03-22 14:00:29 -0600

    calculadoras funcionales

--- app/src/main/java/com/calculadora/CalculadoraAvanzada.kt
+++ app/src/main/java/com/calculadora/CalculadoraAvanzada.kt
@@ -1,21 +1,16 @@
 package com.calculadora
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import java.lang.Exception
-import kotlin.system.exitProcess
 import org.mariuszgromada.math.mxparser.Expression
 
-class MainActivity : AppCompatActivity() {
-    //lateinit var boton: Button
+
+
+class CalculadoraAvanzada : AppCompatActivity() {
     lateinit var texto: TextView
     lateinit var button_1: Button
     lateinit var button_2: Button
@@ -33,12 +28,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var button_multiplicacion: Button
     lateinit var button_division: Button
     lateinit var button_igual: Button
-    lateinit var button_borrar: Button
+    lateinit var button_reset: Button
+    lateinit var button_borrarNum: Button
+    lateinit var button_potencia: Button
+    lateinit var button_raiz: Button
+    lateinit var button_seno: Button
+    lateinit var button_coseno: Button
+    lateinit var button_tangente: Button
+    lateinit var button_parentesisLeft: Button
+    lateinit var button_parentesisRight: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_calculadora_avanzada)
         texto = findViewById(R.id.textSalida)
         button_1 =findViewById(R.id.button_1)
         button_2 =findViewById(R.id.button_2)
@@ -56,8 +59,15 @@ class MainActivity : AppCompatActivity() {
         button_multiplicacion =findViewById(R.id.button_multiplicacion)
         button_division =findViewById(R.id.button_division)
         button_igual =findViewById(R.id.button_igual)
-        button_borrar =findViewById(R.id.button_borrar)
-
+        button_borrarNum =findViewById(R.id.button_resetNum)
+        button_reset =findViewById(R.id.button_reset)
+        button_potencia =findViewById(R.id.button_potencia)
+        button_raiz =findViewById(R.id.button_raiz)
+        button_seno =findViewById(R.id.button_seno)
+        button_coseno =findViewById(R.id.button_coseno)
+        button_tangente =findViewById(R.id.button_tangente)
+        button_parentesisLeft =findViewById(R.id.button_parentesisLeft)
+        button_parentesisRight =findViewById(R.id.button_parentesisRight)
 
         // Asigna a cada botón un escuchador de clics que llamará al método correspondiente de la clase
         button_0.setOnClickListener { presionarDigito(it) }
@@ -76,36 +86,17 @@ class MainActivity : AppCompatActivity() {
         button_resta.setOnClickListener { presionarOperacion(it) }
         button_multiplicacion.setOnClickListener { presionarOperacion(it) }
         button_division.setOnClickListener { presionarOperacion(it) }
+        button_potencia.setOnClickListener { presionarOperacion(it) }
+        button_raiz.setOnClickListener { presionarOperacion(it) }
+        button_seno.setOnClickListener { presionarOperacion(it) }
+        button_coseno.setOnClickListener { presionarOperacion(it) }
+        button_tangente.setOnClickListener { presionarOperacion(it) }
+        button_parentesisLeft.setOnClickListener { presionarOperacion(it) }
+        button_parentesisRight.setOnClickListener { presionarOperacion(it) }
 
         button_igual.setOnClickListener { calcular(it) }
-        button_borrar.setOnClickListener { reset(it) }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-       // return super.onCreateOptionsMenu(menu)
-        val inflater : MenuInflater = menuInflater
-        inflater.inflate(R.menu.main_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.salir -> {
-                exitProcess(0)
-                true
-            }
-            R.id.calculadoraAvanzada -> {
-                var intent = Intent(this, CalculadoraAvanzada::class.java)
-                startActivity(intent)
-                true
-            }
-            R.id.acercade -> {
-                var intent = Intent(this, Acercade::class.java)
-                startActivity(intent)
-                true
-        }
-            else ->return super.onOptionsItemSelected(item)
-        }
+        button_borrarNum.setOnClickListener { borrarNum(it) }
+        button_reset.setOnClickListener { reset(it) }
     }
     fun presionarDigito(view: View) {
         val boton = view as Button
@@ -114,6 +105,7 @@ class MainActivity : AppCompatActivity() {
         val concatenarSinCeros = quitarCerosIzquierda(concatenar)
         texto.text = concatenarSinCeros
     }
+
     fun presionarOperacion(view: View) {
         val boton = view as Button
         val textoBoton = boton.text.toString()
@@ -121,6 +113,7 @@ class MainActivity : AppCompatActivity() {
         val concatenarSinCeros = quitarCerosIzquierda(concatenar)
         texto.text = concatenarSinCeros
     }
+
     fun calcular(view: View) {
         val texto1 = texto.text.toString()
         val expr = Expression(texto1)
@@ -140,5 +133,13 @@ class MainActivity : AppCompatActivity() {
     }
     fun reset(view: View) {
         texto?.text = "0" // se actualiza el texto del TextView con el valor "0"
+    }
+    fun borrarNum(view: View) {
+        val texto1 = texto.text.toString()
+        if (texto1.length > 1) {
+            texto.text = texto1.substring(0, texto1.length - 1)
+        } else {
+            texto.text = "0"
+        }
     }
 }
